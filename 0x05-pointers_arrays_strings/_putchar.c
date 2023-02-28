bf316f70c82957fe7d11ea1cd3bd6e4b7a530d50
Bassam51ezzat <bassam51bassam@gmail.com>
2023-02-28 04:07:25 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unisted.h>
+
+/**
+ * _putchar.c _writes character c to stdout
+ * @c: char to be printed
+ * Return: on success
+ */
+int _putchar.c(char c)
+{
+	return (write(1,&c,1));
+}	
